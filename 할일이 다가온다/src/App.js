29aa25a566109f4b27949f6b1e1b9da123268e02
bumi kim <gimbumi25@gmai.com>
2025-09-24 12:50:17 +0900

    first page

--- 할일이 다가온다/src/App.js
+++ 할일이 다가온다/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+function App() {    //컴포넌트 정의
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //JSX 반환 받기 -> 렌더링
+    //여기까지는 주석이 이렇게 생김
+    <div className="App"> {/*여기부터 이런 형식*/}
+      {" "}
+      {/*JSX는 JS 기반이라 class 써버리면 안됨*/}
+      <div className="black_bg"> {/*부모 컴포넌트가 감싸줘야 오류X + 검은배경 가져옴*/}
+        <h2>할 일이 다가온다...
+        </h2>
+          <button className="start-btn">시작하기</button>
+      </div>
+      
     </div>
   );
 }
